da942a3fc5b5459d408d99b2a28b7919b11d6935
Pablo <pabloalmecija@gmail.com>
2020-06-18 21:59:41 +0200

    Init Spring Bot projects for Product Service and Order Service

---
+++ orders-service/src/test/java/com/yockie/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yockie.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
